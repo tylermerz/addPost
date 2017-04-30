ba69321838631c1a9255032bcd4a6db3a3088a81
Tyler Merz <tyler.merz@gmail.com>
2017-04-30 16:22:42 -0700

    updated readme and barred stop words from being used in the scoring algorithm.

---
+++ README.md
@@ -0,0 +1,22 @@
+# addPost
+This is a Typescript project that I use to help compose, save, and summarize blog posts that I make on my own personal website.
+
+## Installation and Usage
+### Installation
+To install this project just clone it from the repo and then use npm to install it. I have implicitly included a reference to the styling file that I use for my [personalWebsite](https://github.com/tylermerz/personalWebsite) (`views/layouts/edefault.tsx`) to allow for previews in the browser, so change this to whatever your website style requires.
+
+~~~bash
+git clone https://github.com/tylermerz/addPost.git
+cd addPost
+npm install
+~~~
+
+After it has been installed with npm, webpack needs to be called to compile the tsx files into a bundled javascript file.
+
+~~~bash
+webpack --config addPostwebpack.config.js
+~~~
+### Usage
+Simply open `addPost.html` in your favorite web browswer and start composing. The save and load features save files in your browser's localStorage implementation, so they are not secure. Please don't save anything that is secret/confidential.
+
+The automatic summary feature is a naive implementation of [this tool](https://thetokenizer.com/2013/04/28/build-your-own-summary-tool/) where the first figure element of the post is appended to the end of the summary.
--- autoSummarizer.ts
+++ autoSummarizer.ts
@@ -1,7 +1,10 @@
 /**Takes in an HTMLTemplateElement and tries to calculate an automatic summary of the text.
  * Method based on https://thetokenizer.com/2013/04/28/build-your-own-summary-tool/
  */
+//taken from http://xpo6.com/list-of-english-stop-words/
 let stopWordsArray = require("./stopWords.json");
+stopWordsArray = new Set(stopWordsArray);
+
 export default class autoSummarizer{
     version:string;
     versionLink:string;
@@ -17,16 +20,34 @@ export default class autoSummarizer{
         this.numSentences = 0;
         this.version = "1.0";
         this.versionLink = "https://github.com/tylermerz/addPost/blob/master/autoSummarizer.ts";
-        console.log(stopWordsArray);
     }
 
     summarize():string{
         this.splitParagraphs();
         this.scoreSentences();
-        this.compileSummary();
+        this.compileTextSummary();
+        this.compileFigureSummary();
+
+        this.watermarkSummary();
         return this.summary;
     }
-    
+    splitParagraphKeepPunctuation(s1:string){
+        let sentences :Array<string>;
+        sentences = [];
+        let unmatchedString = s1;
+        let match = unmatchedString.match(/([.?!]) /);
+
+        if (match === null){
+            return [s1];//only a 1 sentence paragraph
+        }
+        while (match !== null){
+            sentences.push(unmatchedString.substr(0,match['index']+1));
+            unmatchedString = unmatchedString.substring(match['index']+2);
+            match = unmatchedString.match(/([.?!]) /)
+        }
+        return sentences;
+
+    }
     /**Splits the total original text into paragraph blocks*/
     splitParagraphs(){
         this.paragraphs = [];
@@ -35,7 +56,7 @@ export default class autoSummarizer{
         let textParagraphs = this.originalText.firstElementChild.getElementsByTagName("p");
         for (var i =0;i< textParagraphs.length;i++){
             let sentences = textParagraphs[i].innerText.split(/[.!?] /);
-            this.paragraphsHTML.push(textParagraphs[i].innerHTML.split(/[.!?] /));
+            this.paragraphsHTML.push(this.splitParagraphKeepPunctuation(textParagraphs[i].innerHTML));
             this.paragraphs.push(sentences);
             this.sentenceArray = this.sentenceArray.concat(sentences);
             this.numSentences+=sentences.length;
@@ -57,17 +78,20 @@ export default class autoSummarizer{
                 sentenceNumber++;
             });
         });
-        console.log(this.weightMatrix)
     }
     scoreSentencePair(s1:string,s2:string):number{
         //split into words based on spaces and punctuation
         let s1Words = s1.toLowerCase().split(/[,;:/—–]? /);
         let s2Words = s2.toLowerCase().split(/[,;:/—–]? /);
+        //clean both sentence1 and sentence2 of stop words so those don't contribute to the hits
+        s1Words = s1Words.filter((word)=>{return !stopWordsArray.has(word)});
+        s2Words = s2Words.filter((word)=>{return !stopWordsArray.has(word)});
+
         let s2WordsSet = new Set(s2Words);
         let score = s1Words.filter((word)=>s2WordsSet.has(word)).length;
         return 2 * score/(s1Words.length+s2Words.length);
     }
-    compileSummary(){
+    compileTextSummary(){
         //find the highest score sentence in each paragraph
         let firstSentence = 0;
         this.summary = "<p>";
@@ -77,18 +101,24 @@ export default class autoSummarizer{
             let bestScore = sentenceMatrix.map((row)=>{
                 return Math.max(...row);
             });
-            console.log(bestScore)
             let overallBestScore = Math.max(...bestScore);
             let bestSentenceIndex = bestScore.findIndex((score)=>{return score===overallBestScore});
             let bestSenence = this.paragraphsHTML[paragraphNum][bestSentenceIndex];//add the HTML version 
-            console.log(bestSentenceIndex)
-            console.log(bestSenence)
             this.summary=this.summary+bestSenence+" ";
 
             firstSentence+=paragraph.length;
         });
 
-        this.summary = this.summary +"</p><p><em>Summarized by <a href='"+this.versionLink+"'>autoSummarizer "+this.version+"</a></em></p>";
-        
+        this.summary = this.summary.trim() +"</p>";
+    }
+
+    compileFigureSummary(){
+        //find first figure environment and append it
+
+        let figures = this.originalText.firstElementChild.getElementsByTagName("figure");
+        this.summary = this.summary+"<figure>" + figures[0].innerHTML+"</figure>";
+    }
+    watermarkSummary(){
+        this.summary = this.summary + "<p><em>Summarized by <a href='"+this.versionLink+"'>autoSummarizer "+this.version+"</a></em></p>";
     }
 }
